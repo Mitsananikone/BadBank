c401fefc099fcf82bb1e3defdb2bbdd470e37e16
Mit Sananikone <Mitsananikone@gmail.com>
2023-06-06 17:32:08 -0500

    Delete db.config.js

--- db.config.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = {
-    uri: "mongodb+srv://mitsananikone:Cu48z5a4%21Cu48z5a4%21@bankingappcluster.48tgqmy.mongodb.net/BadBank",
-    apiurl: "http://localhost:3000",
-    admin: "admin"
-  };
-  
-  module.exports = dbConfig;
-  
