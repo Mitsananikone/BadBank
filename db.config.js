c2d9f34b09da17e939e3045ab6686ca7f6abf76a
Mit Sananikone <Mitsananikone@gmail.com>
2023-06-09 17:01:33 -0500

    Implemented dynamic web response

---
+++ db.config.js
@@ -0,0 +1,8 @@
+// const dbConfig = {
+//     uri: "mongodb+srv://mitsananikone:Cu48z5a4%21Cu48z5a4%21@bankingappcluster.48tgqmy.mongodb.net/BadBank",
+//     apiurl: "http://localhost:3000",
+//     admin: "admin"
+//   };
+  
+//   module.exports = dbConfig;
+  
